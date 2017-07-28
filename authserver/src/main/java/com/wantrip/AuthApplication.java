5c95a4428f48cf39f0b9e9937d00f3defa618018
junfeng279 <junfeng279@163.com>
2017-07-28 12:21:15 +0800

    添加了注册中心和网关中心设置

--- authserver/src/main/java/com/wantrip/AuthApplication.java
+++ authserver/src/main/java/com/wantrip/AuthApplication.java
@@ -2,14 +2,16 @@ package com.wantrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by junfeng on 17-7-26.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+    public static void main(String[] args){
+        SpringApplication.run(AuthApplication.class, "--server.port=8081");
     }
 }
---
+++ registry/src/main/java/com/wantrip/registry/Application.java
@@ -0,0 +1,17 @@
+package com.wantrip.registry;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by niujunfeng on 17-4-27.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
